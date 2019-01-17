624121157f91c5f6a74805526c04960eee98d156
George Sakkis <george.sakkis@gmail.com>
2019-01-17 23:43:09 +0200

    Expose itertools.accumulate

--- more_itertools/rich_iterator.py
+++ more_itertools/rich_iterator.py
@@ -1,6 +1,9 @@
 import itertools as it
+import operator
 
 import six
+
+from . import recipes
 
 
 class RichIterator(six.Iterator):
@@ -29,5 +32,11 @@ class RichIterator(six.Iterator):
     def cycle(self):
         return self._wrap(it.cycle)
 
+    def accumulate(self, func=operator.add):
+        return self._wrap(_accumulate, func)
+
     def _wrap(self, func, *args, **kwargs):
         return self.__class__(func(self._it, *args, **kwargs))
+
+
+_accumulate = getattr(it, 'accumulate', recipes.accumulate)
--- more_itertools/tests/test_rich_iterator.py
+++ more_itertools/tests/test_rich_iterator.py
@@ -1,4 +1,5 @@
 import itertools as it
+import operator
 import unittest
 
 from six.moves import range
@@ -10,7 +11,7 @@ class RichIteratorTests(unittest.TestCase):
     """Tests for ``RichIterator()``"""
 
     def setUp(self):
-        rng = range(5)
+        rng = range(1, 6)
         self.rich_iters = list(map(mi.RichIterator, [
             rng,
             list(rng),
@@ -21,9 +22,9 @@ class RichIteratorTests(unittest.TestCase):
     def test_iteration(self):
         """Test basic iteration"""
         for ri in self.rich_iters:
-            self.assertEqual(next(ri), 0)
             self.assertEqual(next(ri), 1)
-            self.assertEqual(list(ri), [2, 3, 4])
+            self.assertEqual(next(ri), 2)
+            self.assertEqual(list(ri), [3, 4, 5])
             self.assertRaises(StopIteration, next, ri)
             self.assertEqual(list(ri), [])
 
@@ -50,4 +51,13 @@ class RichIteratorTests(unittest.TestCase):
     def test_cycle(self):
         for ri in self.rich_iters:
             self.assertEqual(list(it.islice(ri.cycle(), 12)),
-                             [0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1])
+                             [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2])
+
+    def test_accumulate(self):
+        for ri in self.rich_iters:
+            self.assertEqual(list(ri.accumulate()), [1, 3, 6, 10, 15])
+
+    def test_accumulate_op(self):
+        for ri in self.rich_iters:
+            self.assertEqual(list(ri.accumulate(operator.mul)),
+                             [1, 2, 6, 24, 120])
